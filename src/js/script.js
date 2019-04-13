28d0aabda4acfeaebe7bd78d58283d8c7fab84e9
Monika <molinskamonika+github@gmail.com>
2019-04-13 08:59:01 +0200

    modal login

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,8 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
+	const body = document.querySelector('.body');
+
+// mobile menu
 	const mobileMenu = document.querySelector('.mobile-menu');
 	const headerNav = document.querySelector('.header__nav');
 	const navList = document.querySelector('.nav__list');
@@ -7,7 +10,33 @@ window.addEventListener('DOMContentLoaded', (event) => {
 	mobileMenu.addEventListener('click', () => {
 		headerNav.classList.toggle('header__nav--open');
 		navList.classList.toggle('nav__list--open');
-	})
+	});
+
+// modal popup
+	const login = document.querySelector('.btn--login');
+	const modal = document.querySelector('.modal');
+	const contentWrapper = document.querySelector('.content__wrapper');
+	const closeModalBtn = document.querySelector('.modal__close-btn')
+
+	toggleModal = () => {
+		modal.classList.toggle('modal--show');
+	}
+
+	blurBackground = () => {
+		contentWrapper.classList.toggle('opacity');
+	}
+
+	login.addEventListener('click', () => {
+		toggleModal();
+		blurBackground();
+	});
+
+	console.log(closeModalBtn);
+
+	closeModalBtn.addEventListener('click', () => {
+		toggleModal();
+		blurBackground();
+	});
 
 });
 
